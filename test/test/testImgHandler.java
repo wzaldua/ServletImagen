947ac2c79d78f0171b9b6a71a0c76879760f6b9b
wilvergzalduae <wilvergzalduae@192.168.1.60>
2014-08-22 00:33:07 -0500

    Primeras pruebas

---
+++ test/test/testImgHandler.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package test;
+
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author wilvergzalduae
+ */
+public class testImgHandler {
+    
+    @Test
+    public void archivoExiste(){
+        
+    }
+    
+}
